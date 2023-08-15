7ea07e25b2bed4be3842c6f6544fbc01e5677552
Mikaela Yasay <97491593+MikaelaYasay@users.noreply.github.com>
2023-08-14 15:00:55 -1000

    add more content and context

--- js/script.js
+++ js/script.js
@@ -28,26 +28,47 @@ document.querySelectorAll(".animate-on-scroll").forEach((section) => {
   myobserver.observe(section);
 });
 
-// // REVEAL ON SCROLL
-// // Detect request animation frame
-// var scroll = window.requestAnimationFrame ||
+// REVEAL ON SCROLL
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
-// // IE Fallback
-// function(callback){ window.setTimeout(callback, 1000/60)};
-// var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+function loop() {
 
-// function loop() {
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
+    });
 
-//   Array.prototype.forEach.call(elementsToShow, function(element){
-//     if (isElementInViewport(element)) {
-//       element.classList.add('is-visible');
-//     } else {
-//       element.classList.remove('is-visible');
-//     }
-//   });
+    scroll(loop);
+}
 
-//   scroll(loop);
-// }
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
 
 // CHANGE HOME BUTTON ON HOVER
 $(function(){
